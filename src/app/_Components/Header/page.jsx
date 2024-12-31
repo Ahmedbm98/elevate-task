c20f47b74f9eec3794a302f8c5f18abd69b49adf
Ahmed Abdel basset Mohamed <107061768+Ahmedbm98@users.noreply.github.com>
2024-12-31 19:48:20 +0200

    second edite

--- src/app/_Components/Header/page.jsx
+++ src/app/_Components/Header/page.jsx
@@ -3,7 +3,6 @@ export default function Header() {
     <>
       <header
         className={` shadow-sm py-4 px-3 bg-whiteColor`}
-        data-aos="zoom-in"
       >
         <h1 className="max-xl:text-3xl  text-5xl font-semibold  text-gray-900 text-center ">
           👋 Hi <span className="text-maincolor my-animate ">Elevate Tech</span>{" "}
